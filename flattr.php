ec037c2d34043ba5a0caf57006488c7011bc9167
flattr <flattr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-26 07:38:46 +0000

    added license
    git-svn-id: https://plugins.svn.wordpress.org/flattr/trunk@280874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flattr.php
+++ flattr.php
@@ -6,6 +6,7 @@ Description: Give your readers the opportunity to Flattr your effort
 Version: 0.9.12
 Author: Flattr.com
 Author URI: http://flattr.com/
+License: This code is (un)licensed under the kopimi (copyme) non-license; http://www.kopimi.com. In other words you are free to copy it, taunt it, share it, fork it or whatever. :)
 */
 
 class Flattr
