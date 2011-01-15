15d6980ef92cd9862a77c1d9f27f816cb00fc957
aphex3k <aphex3k@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-15 08:45:33 +0000

    changed plugins developer nick name to real name
    git-svn-id: https://plugins.svn.wordpress.org/flattr/trunk@332795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flattr.php
+++ flattr.php
@@ -4,10 +4,11 @@ Plugin Name: Flattr
 Plugin URI: http://wordpress.org/extend/plugins/flattr/
 Description: Give your readers the opportunity to Flattr your effort
 Version: 0.9.19
-Author: aphex3k
+Author: Michael Henke
 Author URI: http://allesblog.de/
 License: This code is (un)licensed under the kopimi (copyme) non-license; http://www.kopimi.com. In other words you are free to copy it, taunt it, share it, fork it or whatever. :)
-*/
+Comment: The author of this plugin is not affiliated with the flattr company in whatever meaning.
+ */
 
 if (version_compare(PHP_VERSION, '5.0.0', '<'))
 {
