d43f15dcde013a9d65b12192c4a36cfcff192661
aphex3k <aphex3k@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-02 10:44:53 +0000

    PHP 5.2 compatible
    git-svn-id: https://plugins.svn.wordpress.org/flattr/trunk@499058 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flattr_client.php
+++ flattr_client.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__ . '/lib/httpconnection.php';
-require_once __DIR__ . '/lib/httpresponse.php';
-require_once __DIR__ . '/lib/oauth2client.php';
+require_once dirname(__FILE__) . '/lib/httpconnection.php';
+require_once dirname(__FILE__) . '/lib/httpresponse.php';
+require_once dirname(__FILE__) . '/lib/oauth2client.php';
